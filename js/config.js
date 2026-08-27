d50e3a4a78d6ae8df14e3b0cd09386fe5d70aef7
2gmcompany <geovanimahumane@gmail.com>
2026-08-27 13:14:20 +0200

    Update Instagram URL in configuration

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 /* Configuração de publicação. Preencha apenas o endpoint do serviço escolhido. */
 window.ERGOVIDA_CONFIG = {
   formEndpoint: '', // Ex.: https://formspree.io/f/SEU_CODIGO
-  instagramUrl: ''  // Adicione apenas a URL oficial confirmada pela clínica.
+  instagramUrl: https://www.instagram.com/centromedicoergovida/''  // Adicione apenas a URL oficial confirmada pela clínica.
 };
