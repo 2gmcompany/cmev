2906dc38074e42390288a8c3bcaf21ef875dbd42
2gmcompany <geovanimahumane@gmail.com>
2026-08-27 13:37:53 +0200

    Fix config syntax and add Facebook URL

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 /* Configuração de publicação. Preencha apenas o endpoint do serviço escolhido. */
 window.ERGOVIDA_CONFIG = {
-  formEndpoint: '', // Ex.: https://formspree.io/f/SEU_CODIGO
-  instagramUrl: https://www.instagram.com/centromedicoergovida/''  // Adicione apenas a URL oficial confirmada pela clínica.
+  formEndpoint: '', // Ex.: 'https://formspree.io/f/SEU_CODIGO'
+  instagramUrl: 'https://www.instagram.com/centromedicoergovida/',
+  facebookUrl: 'https://www.facebook.com/profile.php?id=61584021944318'
 };
