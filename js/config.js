d100b145918882cc2d1f97233d24c933f2e90e3b
2gmcompany <geovanimahumane@gmail.com>
2026-09-10 11:32:39 +0200

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-/* Configuração de publicação. Preencha apenas o endpoint do serviço escolhido. */
-window.ERGOVIDA_CONFIG = {
-  formEndpoint: '', // Ex.: 'https://formspree.io/f/SEU_CODIGO'
-  instagramUrl: 'https://www.instagram.com/centromedicoergovida/',
-  facebookUrl: 'https://www.facebook.com/profile.php?id=61584021944318'
-};
